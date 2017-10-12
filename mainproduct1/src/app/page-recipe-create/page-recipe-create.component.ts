c33a498d9862b1d6a7055a5f0f79e1de53278401
Jeremy Yung Hau Ng <jeremy.ng1002@gmail.com>
2017-10-12 20:29:22 +1000

    style modification for page recipe

--- mainproduct1/src/app/page-recipe-create/page-recipe-create.component.ts
+++ mainproduct1/src/app/page-recipe-create/page-recipe-create.component.ts
@@ -3,6 +3,8 @@ import { FormGroup, FormBuilder, FormControl, Validators, FormArray } from '@ang
 import { recipe } from '../classes/recipe';
 import { recipeList } from '../data/recipe-list';
 import { Router } from '@angular/router';
+import { ingredientLibrary } from '../data/ingredient-library';
+import { Ingredient } from '../classes/ingredient';
 
 @Component({
   selector: 'app-page-recipe-create',
@@ -11,6 +13,17 @@ import { Router } from '@angular/router';
 })
 export class PageRecipeCreateComponent implements OnInit {
   recipe: recipe;
+  // Ingredients 
+  meats: Ingredient[] = [];
+  seafoods: Ingredient[] = [];
+  vegetables: Ingredient[] = [];
+  fruits: Ingredient[] = [];
+  meatType: boolean = false;
+  seafoodType: boolean = false;
+  vegetableType: boolean = false;
+  fruitType: boolean = false;
+
+  // For the form
   recipeForm: FormGroup;
   name: string;
   description: string;
@@ -24,47 +37,73 @@ export class PageRecipeCreateComponent implements OnInit {
   }
 
   ngOnInit() {
+    for(let i = 0; i < ingredientLibrary.length; i++){
+      if (ingredientLibrary[i].type == 'meat') {
+        this.meats.push(ingredientLibrary[i])
+      } else if (ingredientLibrary[i].type == 'seafood') {
+        this.seafoods.push(ingredientLibrary[i])
+      } else if (ingredientLibrary[i].type == 'vegetable') {
+        this.vegetables.push(ingredientLibrary[i])
+      } else if (ingredientLibrary[i].type == 'fruit') {
+        this.fruits.push(ingredientLibrary[i])
+      }
+    }
+  }
+
+  show(type: string){
+    if(type == 'meat') {
+      this.meatType = this.meatType ? false : true;
+      this.seafoodType = false;
+      this.vegetableType = false;
+      this.fruitType = false;
+    } else if(type == 'seafood') {
+      this.meatType = false;
+      this.seafoodType = this.seafoodType ? false : true;
+      this.vegetableType = false;
+      this.fruitType = false;
+    } else if(type == 'vegetable') {
+      this.meatType = false;
+      this.seafoodType = false;
+      this.vegetableType = this.vegetableType ? false : true;
+      this.fruitType = false;
+    } else if(type == 'fruit') {
+      this.meatType = false;
+      this.seafoodType = false;
+      this.vegetableType = false;
+      this.fruitType = this.fruitType ? false : true;
+    }
   }
 
   buildForm() {
     this.recipeForm = this._fb.group({
       recipeName: ['', [Validators.required, Validators.minLength(3)]],
       recipeDescription: ['', [Validators.required, Validators.minLength(5)]],
-      recipeInstructions: this._fb.array([
-        this.initInstruction(),
-      ]),
-    })
-  }
-
-  initInstruction() {
-    return this._fb.group({
-      instruction: ['', [Validators.required,, Validators.minLength(3)]]
+      recipeInstructions: [[]],
+      instruction: ['',[Validators.required]],
+      // recipeInstructions: [[], [Validators.required]],
+      // recipeIngredient: ['', Validators.required]
     })
   }
 
   addInstruction() {
-    // add address to the list
-    const control = <FormArray>this.recipeForm.controls['recipeInstructions'];
-    control.push(this.initInstruction());
-  }
-
-  removeInstruction(index: number) {
-    // remove address from the list
-    const control = <FormArray>this.recipeForm.controls['recipeInstructions'];
-    control.removeAt(index);
+     let instructions = this.recipeForm.get('recipeInstructions').value;
+     console.log(instructions)
+     instructions.push(this.recipeForm.get('instruction').value);
+     console.log(instructions)
   }
 
   onSubmitForm(recipeForm) {
     this.name = this.recipeForm.get('recipeName').value;
     this.description = this.recipeForm.get('recipeDescription').value;
     this.instructions = this.recipeForm.get('recipeInstructions').value;
+    console.log(this.instructions)
     this.recipe = {
       name: this.name,
       instruction: this.instructions,
       description: this.description,
       sharedRecipe: false,
       ingredients: [''],
-      imgURL: ''
+      imgURL: './assets/images/recipes/lunchbox.png'
     }
     recipeList.push(this.recipe);
     console.log(recipeList);
@@ -72,3 +111,4 @@ export class PageRecipeCreateComponent implements OnInit {
   }
 
 }
+
