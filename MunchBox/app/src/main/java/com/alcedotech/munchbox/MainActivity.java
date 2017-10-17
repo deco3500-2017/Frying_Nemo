34e4585a66607946811b0d82c26ff2799629daab
Kelvin Ding <weisheng5566@gmail.com>
2017-10-17 23:32:53 +1000

    Added Android application for MunchBox

---
+++ MunchBox/app/src/main/java/com/alcedotech/munchbox/MainActivity.java
@@ -0,0 +1,58 @@
+package com.alcedotech.munchbox;
+
+import android.net.http.SslError;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = (WebView)findViewById(R.id.webview);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setAllowContentAccess(true);
+        webView.getSettings().setAppCacheEnabled(true);
+        webView.getSettings().setAllowFileAccess(true);
+
+        webView.setHorizontalScrollBarEnabled(false);
+        webView.setOnTouchListener(new View.OnTouchListener() {
+            float m_downX;
+            public boolean onTouch(View v, MotionEvent event) {
+
+                if (event.getPointerCount() > 1) {
+                    //Multi touch detected
+                    return true;
+                }
+
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN: {
+                        // save the x
+                        m_downX = event.getX();
+                        break;
+                    }
+                    case MotionEvent.ACTION_MOVE:
+                    case MotionEvent.ACTION_CANCEL:
+                    case MotionEvent.ACTION_UP: {
+                        // set x so that it doesn't move
+                        event.setLocation(m_downX, event.getY());
+                        break;
+                    }
+
+                }
+                return false;
+            }
+        });
+
+        webView.loadUrl("https://munch-box.appspot.com/");
+
+
+    }
+}
