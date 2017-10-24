13e0581bc07bd348bad25bd08a58d1ed0e70bbf5
Jeremy Yung Hau Ng <jeremy.ng1002@gmail.com>
2017-10-24 11:53:15 +1000

    updating the profile

--- mainproduct1/src/app/data/fake-user.ts
+++ mainproduct1/src/app/data/fake-user.ts
@@ -2,8 +2,8 @@ import { user } from '../classes/user';
 
 export const allUsers: user[] = [
     {
-        name: 'Woody Sins',
-        gender: 'Male',
+        name: 'Ashley Simpson',
+        gender: 'Female',
         age: 11,
         weight: 31,
         height: 120
